cc1fb76b496855850b3666204935cb827b45afe1
endercraftm <enderdeletouocanalantigo@gmail.com>
2024-09-07 17:05:23 -0300

    Testes e criação de variáveis

--- Golden Ages/ReatorNuclear.lua
+++ Golden Ages/ReatorNuclear.lua
@@ -1,2 +1,20 @@
+-- Reator Novo Mundo
+
+versao = 0.1 
+
 os.unloadAPI("sensors")
 os.loadAPI("/rom/apis/sensors")
+
+local side = sensors.getController()
+local sensor = sensors.getSensors(side)
+local probe = sensors.getProbes(side, sensor[1])
+local target = sensors.getAvailableTargetsforProbe(side, sensor[1], probe[2])
+local reading = sensors.getSensorReadingAsDict(side, sensor[1], target[1], probe[2])
+local infos = {reading.heat,reading.size,reading.output}
+
+-- Testes temporários
+
+print("  [ Infos ]\n")
+print("Calor: "..tostring(infos[1]))
+print("Tamanho: "..tostring(infos[2]))
+print("Saida de Energia: "..tostring(infos[3])) 
