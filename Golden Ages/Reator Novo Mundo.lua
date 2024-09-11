6c352b1fa8174a912ae2b790ec92c79a4fbf78cd
endercraftm <enderdeletouocanalantigo@gmail.com>
2024-09-11 00:20:28 -0300

    Update Reator

--- Golden Ages/Reator Novo Mundo.lua
+++ Golden Ages/Reator Novo Mundo.lua
@@ -2,7 +2,27 @@
 
 --versao = 0.2
 
+--[[    
+
+    Lista de coisas para fazer
+
+    -> Sistema de Resfriamento
+    -> Alarme
+    -> Menus
+    -> Modo Manual
+    -> Calor Máximo ( 5000+1000*reading.size )
+
+]]--
+
 local inicio = true
+
+function limpa_linhas(l1,l2,l3)
+    for i = l1, l2 do
+    term.setCursorPos(1,i)
+    term.clearLine()
+    end
+    term.setCursorPos(1,l3)
+end
 
 function sair_programa()
 
@@ -21,57 +41,39 @@ end
 function reator()
 
     while inicio do
-
-    os.unloadAPI("sensors")
-    os.loadAPI("/rom/apis/sensors")
-
-    side = sensors.getController()
-    sensor = sensors.getSensors(side)
-    probe = sensors.getProbes(side, sensor[1])
-    target = sensors.getAvailableTargetsforProbe(side, sensor[1], probe[2])
-    reading = sensors.getSensorReadingAsDict(side, sensor[1], target[1], probe[2])
-    infos = {reading.heat,reading.size,reading.output}
+    
+    local target = sensors.getAvailableTargetsforProbe("left", "Sensor", "Reactor")
+    local reading = sensors.getSensorReadingAsDict("left", "Sensor", target[1], "Reactor")
     local status = {"Ativado","Desativado","Resfriando...","Resfriamento Concluido"}
+    local status_atual
 
-    term.setCursorPos(1,1)
-    term.clearLine()
-    print("Calor: "..tostring(infos[1]))
-    print("Reator: "..tostring(status[2]))
-    term.setCursorPos(1,3)
-    term.clearLine()
-    print("Saida de EU: "..tostring(infos[3]))
+    local valor1 = 750  -- Variaveis temporárias
+    local valor2 = valor1*4 -- Variaveis temporárias
 
-        if infos[1] == 0 then -- Calor
+    if reading.heat == 0 then
+        rs.setBundledOutput("back",colors.white)
+        elseif reading.heat > 0 and reading.heat <= valor1 then
+        rs.setBundledOutput("back",colors.combine(colors.yellow))
+        elseif reading.heat > valor1 and reading.heat < valor2 then
+        rs.setBundledOutput("back",colors.combine(colors.yellow,colors.orange))
+        elseif reading.heat >= valor2 then 
+        rs.setBundledOutput("back",colors.combine(colors.yellow,colors.orange,colors.red))
+    end
+    
+    if rs.testBundledInput("back",colors.lime) then
+        status_atual = status[1]
+        elseif not rs.testBundledInput("back",colors.lime) then
+        status_atual = status[2]
+        elseif rs.testBundledInput("back",colors.combine(colors.lightBlue,colors.cyan)) then
+        status_atual = status[3]
+        elseif rs.testBundledInput("back",colors.white) then
+        status_atual = status[4]
+    end
 
-            rs.setBundledOutput("back",colors.white)
-
-            elseif infos[1] > 0 and infos[1] <= 3000 then
-
-            rs.setBundledOutput("back",colors.yellow)
-
-            elseif infos[1] > 3000 and infos[1] <= 6000 then
-
-            rs.setBundledOutput("back",colors.combine(colors.yellow,colors.orange))
-
-            elseif infos[1] > 6000 then
-
-            rs.setBundledOutput("back",colors.combine(colors.yellow,colors.orange,colors.red))
-
-        end
-
-        if rs.testBundledInput("back",colors.green) and rs.testBundledInput("back",colors.lime) then --Reator Ligado/Desligado
-
-            term.setCursorPos(1,2)
-            term.clearLine()
-            print("Reator: "..tostring(status[1]))
-
-            elseif not rs.testBundledInput("back",colors.green) and not rs.testBundledInput("back",colors.lime) then
-
-            term.setCursorPos(1,2)
-            term.clearLine()
-            print("Reator: "..tostring(status[2]))
-            
-        end
+    limpa_linhas(1,1,1)
+    print("Status do Reator: "..tostring(status_atual))
+    limpa_linhas(2,2,2)
+    print("Calor: "..tostring(reading.heat))
 
     os.sleep(0.1)
     end
@@ -79,4 +81,5 @@ function reator()
 end
 
 term.clear()
+term.setCursorPos(1,1)
 parallel.waitForAll(reator,sair_programa)
